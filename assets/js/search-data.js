577254dd6577315734016abbe56bdae201e517f5
sanjanb <159162942+sanjanb@users.noreply.github.com>
2025-09-13 01:05:04 +0000

    Deploying to gh-pages from @ sanjanb/sanjanb.github.io@ee6daed458dc167a9467c3b7b13c0cf167d31017 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,14 +31,14 @@ ninja.data = [{
             window.location.href = "/achievements/";
           },
         },{id: "nav-repositories",
-          title: "repositories",
+          title: "Repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
@@ -174,11 +174,6 @@ ninja.data = [{
           description: "",
           section: "Work",handler: () => {
               window.location.href = "/work/ecommerce-platform-modernization/";
-            },},{id: "work-ai-powered-data-analytics-platform",
-          title: 'AI-Powered Data Analytics Platform',
-          description: "",
-          section: "Work",handler: () => {
-              window.location.href = "/work/ai-powered-analytics-platform/";
             },},{id: "work-ai-powered-cyber-threat-intelligence-system",
           title: 'AI-Powered Cyber Threat Intelligence System',
           description: "",
