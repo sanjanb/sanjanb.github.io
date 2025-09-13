b11d3d2943778dc01a128c18282a312578f89300
sanjanb <159162942+sanjanb@users.noreply.github.com>
2025-09-13 00:20:17 +0000

    Deploying to gh-pages from @ sanjanb/sanjanb.github.io@758e2f53f201cd7af7ca262448053830f4dcbd8e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,7 +31,7 @@ ninja.data = [{
             window.location.href = "/achievements/";
           },
         },{id: "nav-repositories",
-          title: "Repositories",
+          title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
